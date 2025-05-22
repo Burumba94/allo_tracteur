a890e3c997a53061d511791ae8a77c60488de1eb
AG1717 <gassamaaboubacar9@gmail.com>
2025-05-22 14:51:10 +0000

    fix: module in the vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js (en CommonJS)
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
