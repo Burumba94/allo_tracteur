6991521b7d1351893b1934613e676fa3cc1e6838
AG1717 <gassamaaboubacar9@gmail.com>
2025-05-31 01:45:26 +0000

    third fix the * in server

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,8 @@ const corsOptions = {
       callback(new Error(`⛔ Origine non autorisée : ${origin}`));
     }
   },
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   optionsSuccessStatus: 204
 };
 
