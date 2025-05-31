4cd2b936e40113ee5b4bbdd063bc4057f213f3a8
AG1717 <gassamaaboubacar9@gmail.com>
2025-05-31 23:40:37 +0000

    fix: set

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middleware pour gérer les requêtes OPTIONS
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://allo-tracteur.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(200);
-});
+app.options('*', cors(corsOptions))
 
 app.use(express.json());
 app.use('/api/payment', paymentRouter);
