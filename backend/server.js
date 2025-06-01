333c776ed9d102f249c8282598f804c93527adec
AG1717 <gassamaaboubacar9@gmail.com>
2025-06-01 00:30:27 +0000

    fix: splat removed /*

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middleware pour gérer les requêtes OPTIONS
-app.options('/:splat(*)', cors(corsOptions))
+app.options('/*', cors(corsOptions))
 
 app.use(express.json());
 app.use('/api/payment', paymentRouter);
