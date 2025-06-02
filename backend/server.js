ffc70c11c91eb2b1cb6cdc67b66b67b6a9bd7f8f
AG1717 <gassamaaboubacar9@gmail.com>
2025-06-02 12:05:36 +0000

    update env var

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://allo-tracteur.vercel.app',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
